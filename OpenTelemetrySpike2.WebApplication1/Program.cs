0db274f6423ecabf442eb4a05eb75321369c704a
bob hall <robert.hall@4thu.co.uk>
2023-09-25 16:28:16 +0100

    adding serivce-worker

--- OpenTelemetrySpike2.WebApplication1/Program.cs
+++ OpenTelemetrySpike2.WebApplication1/Program.cs
@@ -1,4 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services
+	.AddHostedService<OpenTelemetrySpike2.WebApplication1.Worker>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
